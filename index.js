688a30f12740634c5ae6615a391ec92bab9351c2
jonschlinkert <dev@sellside.com>
2014-06-30 03:26:27 -0400

    fix license

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/**
+ * ## .isObject
+ *
+ * is the value an object, and not an array?
+ *
+ * @param  {*} `value`
+ * @return {Boolean}
+ */
+
 module.exports = function isObject(obj) {
-  return typeof obj === 'object' && !Array.isArray(obj) && obj !== null
+  return typeof obj === 'object'
+    && !Array.isArray(obj)
+    && obj !== null;
 };
