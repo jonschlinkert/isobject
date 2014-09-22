4ce0aca97e60dbcbbc574c467f9da0b17355c257
jonschlinkert <dev@sellside.com>
2014-09-22 10:31:13 -0400

    minor edits, run verb

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-/**
- * ## .isObject
+/*!
+ * isobject <https://github.com/jonschlinkert/isobject>
  *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+/**
  * is the value an object, and not an array?
  *
  * @param  {*} `value`
  * @return {Boolean}
  */
 
-module.exports = function isObject(obj) {
-  return typeof obj === 'object'
-    && !Array.isArray(obj)
-    && obj !== null;
+module.exports = function isObject(o) {
+  return o != null && typeof o === 'object'
+    && !Array.isArray(o);
 };
