02fb7c7ffc6eabb580fc7633b00058b7f07fcee3
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 02:24:14 -0500

    adds tests/docs

--- test.js
+++ test.js
@@ -12,26 +12,18 @@ it('should be true when the value is an object.', function () {
   assert(isObject({}) === true);
   assert(isObject(Object.create({})) === true);
   assert(isObject(Object.create(Object.prototype)) === true);
+  assert(isObject(Object.create(null)) === true);
   assert(isObject({foo: 'bar'}) === true);
-  assert(isObject({}) === true);
 
   function Foo() {this.abc = {};};
-
-  assert(isObject(/foo/) === true);
   assert(isObject(new Foo) === true);
-  assert(isObject(Object.create(null)) === true);
-
+  assert(isObject(/foo/) === true);
 });
 
 it('should be true when the value is not an object.', function () {
   assert(isObject(function () {}) === false);
   assert(isObject(1) === false);
   assert(isObject(['foo', 'bar']) === false);
-  assert(isObject([]) === false);
   assert(isObject(null) === false);
   assert(isObject([]) === false);
-});
-
-it('should be true when the value is null.', function () {
-  assert(isObject(null) === false);
 });
