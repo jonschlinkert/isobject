1354a8e36bf3bbefaed535478d0e58cd53f4a747
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 02:17:24 -0500

    run update

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 /*!
  * isobject <https://github.com/jonschlinkert/isobject>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 var expect = require('chai').expect;
-var isObject = require('../');
+var isObject = require('./');
 
 it('when null is passed it should return false.', function () {
   expect(isObject(null)).to.eql(false);
