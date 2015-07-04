2534dba901412409277f639ec8670bac8fe6261e
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-04 18:46:42 -0400

    adds unit tests

--- test.js
+++ test.js
@@ -1,10 +1,4 @@
-/*!
- * isobject <https://github.com/jonschlinkert/isobject>
- *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
- */
-
+/* deps: mocha */
 var assert = require('assert');
 var isObject = require('./');
 
@@ -19,13 +13,14 @@ it('should be true when the value is an object.', function () {
   assert(isObject(new Foo()));
 });
 
-it('should be true when the value is not an object.', function () {
-  function Foo() {}
+it('should be false when the value is not an object.', function () {
   assert(!isObject('whatever'));
   assert(!isObject(/foo/));
   assert(!isObject(1));
-  assert(!isObject(['foo', 'bar']));
-  assert(!isObject([]));
   assert(!isObject(function () {}));
+  assert(!isObject([]));
+  assert(!isObject(['foo', 'bar']));
+  assert(!isObject());
+  assert(!isObject(undefined));
   assert(!isObject(null));
 });
