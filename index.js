f33b624803fa6e903d93be39387ff1c8d8b12c63
Magnús Dæhlen <sungamd@iterate.no>
2016-11-12 08:18:46 +0100

    Use Array.isArray instead of isarray package

--- index.js
+++ index.js
@@ -7,8 +7,6 @@
 
 'use strict';
 
-var isArray = require('isarray');
-
 module.exports = function isObject(val) {
-  return val != null && typeof val === 'object' && isArray(val) === false;
+  return val != null && typeof val === 'object' && Array.isArray(val) === false;
 };
