a6be22c773154389586d977fb6b6ed16acc8dac5
Matthew Wagerfield <matthew@wagerfield.com>
2018-03-19 09:41:17 +0000

    replaced Array.isArray with isarray package

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 'use strict';
 
+var isArray = require('isarray');
+
 module.exports = function isObject(val) {
-  return val != null && typeof val === 'object' && Array.isArray(val) === false;
+  return val != null && typeof val === 'object' && !isArray(val);
 };
