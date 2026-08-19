c00a62e05777aa6bda7212cdd76e5d75e4a0e3e0
Bogdan Chadkin <opensource@trysound.io>
2026-08-20 01:23:51 +0200

    test: run CommonJS build with Mocha

--- test.js
+++ test.js
@@ -3,20 +3,24 @@
 var assert = require('assert');
 var isObject = require('./');
 
-assert(isObject({}));
-assert(isObject(Object.create({})));
-assert(isObject(Object.create(Object.prototype)));
-assert(isObject(Object.create(null)));
-assert(isObject(/foo/));
+it('should be true when the value is an object.', function() {
+  assert(isObject({}));
+  assert(isObject(Object.create({})));
+  assert(isObject(Object.create(Object.prototype)));
+  assert(isObject(Object.create(null)));
+  assert(isObject(/foo/));
 
-function Foo() {}
-assert(isObject(new Foo()));
+  function Foo() {}
+  assert(isObject(new Foo()));
+});
 
-assert(!isObject('whatever'));
-assert(!isObject(1));
-assert(!isObject(function() {}));
-assert(!isObject([]));
-assert(!isObject(['foo', 'bar']));
-assert(!isObject());
-assert(!isObject(undefined));
-assert(!isObject(null));
+it('should be false when the value is not an object.', function() {
+  assert(!isObject('whatever'));
+  assert(!isObject(1));
+  assert(!isObject(function() {}));
+  assert(!isObject([]));
+  assert(!isObject(['foo', 'bar']));
+  assert(!isObject());
+  assert(!isObject(undefined));
+  assert(!isObject(null));
+});
